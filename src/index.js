da674759ed1bfecf814768e3f0086c6739357823
tanelam <tania.e.aparicio@gmail.com>
2018-03-09 10:50:55 -0500

    refactored version

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 class App {
   constructor() {
-
+  this.beerList = document.getElementById("list-group")
+  this.beerDiv = document.getElementById("beer-detail")
   }
 
   fetchBeers(){
@@ -30,42 +31,39 @@ class App {
   }
 
   renderBeers(beers){
-    let beerList = document.getElementById("list-group")
     // console.log(beers)
     for(let i = 0; i < beers.length; i++){
       let beer = beers[i]
       // console.log(beer)
-
       let beerLi = document.createElement("li")
       beerLi.setAttribute("class", "list-group-item")
       beerLi.innerText = beer.name
-      beerList.append(beerLi)
+      this.beerList.append(beerLi)
 
       beerLi.addEventListener("click", () => {
-        let beerDiv = document.getElementById("beer-detail")
-        beerDiv.innerHTML = ""
+        this.beerDiv.innerHTML = ""
         let beerName = document.createElement("h1")
         beerName.innerText = beer.name
-        beerDiv.append(beerName)
+        this.beerDiv.append(beerName)
 
         let beerImg = document.createElement("img")
         beerImg.setAttribute("src", beer.image_url)
-        beerDiv.append(beerImg)
+        this.beerDiv.append(beerImg)
 
         let tagline = document.createElement("h3")
         tagline.innerText = beer.tagline
-        beerDiv.append(tagline)
+        this.beerDiv.append(tagline)
 
         let textArea = document.createElement("textarea")
         textArea.innerText = beer.description
-        beerDiv.append(textArea)
+        this.beerDiv.append(textArea)
 
         let beerButton = document.createElement("button")
         beerButton.setAttribute("id", "edit-beer")
         beerButton.setAttribute("class", "btn btn-info")
         beerButton.innerText = "Save"
-        beerDiv.append(beerButton)
-          //console.log(beer.name)
+        this.beerDiv.append(beerButton)
+          // console.log(beer.name)
           beerButton.addEventListener("click", () => {
             let text = textArea.value
             let id = beer.id
@@ -74,5 +72,4 @@ class App {
       })
     }
   }
-
 }
